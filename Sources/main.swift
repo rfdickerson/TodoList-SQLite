2e674729c50e4b68113e0129a8adc27b074cb8ff
jsphyin <jsphyin@gmail.com>
2016-06-29 17:00:00 -0500

    Added more dynamic way of getting database configuration

--- Sources/main.swift
+++ Sources/main.swift
@@ -21,21 +21,46 @@ import HeliumLogger
 import LoggerAPI
 import TodoListWeb
 import CloudFoundryEnv
+import TodoListAPI
+
 
 Log.logger = HeliumLogger()
 
-let config = Configuration.sharedInstance
-config.loadCloudFoundry()
+
+
+extension DatabaseConfiguration {
+    
+    init(withService: Service) {
+        if let credentials = withService.credentials{
+            self.host = credentials["host"].stringValue
+            self.username = credentials["username"].stringValue
+            self.password = credentials["password"].stringValue
+            self.port = UInt16(credentials["port"].stringValue)!
+        } else {
+            self.host = "127.0.0.1"
+            self.username = nil
+            self.password = nil
+            self.port = UInt16(5984)
+        }
+        self.options = ["test" : "test"]
+    }
+}
+
+if let service = try CloudFoundryEnv.getAppEnv().getService(spec: "TodoList-Cloudant"){
+    
+let databaseConfiguration = DatabaseConfiguration(withService: service)
+} else {
+    Log.info("Could not find Bluemix Cloudant service")
+}
 
 let todos: TodoList
-if let dbConfig = config.databaseConfiguration {
-    todos = TodoList(dbConfig)
-} else {
-    todos = TodoList()
-}
+
+todos = TodoList()
+
 
 let controller = TodoListController(backend: todos)
 
-let server = HTTPServer.listen(port: config.port, delegate: controller.router)
-Server.run()
-Log.info("Server started on \(config.url).")
+Kitura.addHTTPServer(onPort: 8090, with: controller.router)
+Kitura.run()
+//Server.run()
+//Log.info("Server started on \(config.url).")
