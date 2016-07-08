3fba74ed5874dac3e155644309f4d4ac4779bffd
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-07-08 15:43:01 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "todolist-boilerplate",
+    name: "TodoList",
     dependencies: [
          .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 19),
          .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 9),
