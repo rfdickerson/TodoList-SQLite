33764c831407ff215ec165f1084c6bb32c943175
Robert F. Dickerson <rfdickerson@gmail.com>
2016-07-21 19:36:08 -0500

    Update to 6-20

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "TodoList",
     dependencies: [
-         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 19),
-         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 9),
-         .Package(url: "https://github.com/IBM-Swift/todolist-web", majorVersion: 0),
-         .Package(url: "https://github.com/IBM-Swift/todolist-api", majorVersion: 0),
+         .Package(url: "https://github.com/IBM-Swift/Kitura.git",       majorVersion: 0, minor: 22),
+         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 12),
+         .Package(url: "https://github.com/IBM-Swift/todolist-web",     majorVersion: 0, minor: 3),
+         .Package(url: "https://github.com/IBM-Swift/todolist-api",     majorVersion: 0, minor: 3),
     ]
 )
