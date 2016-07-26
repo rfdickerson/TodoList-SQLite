2c53bb0e8894d8710ca24a3b8e4acb89d7ca2ff1
saiHemak <sakandur@in.ibm.com>
2016-07-26 13:01:53 +0530

    Initial load of SQLite-todo-boilerplate implementation

--- Sources/TodoList.swift
+++ Sources/TodoList.swift
@@ -16,41 +16,210 @@
 
 import Foundation
 import TodoListAPI
+import LoggerAPI
+import SQLite
+import SwiftyJSON
 
 struct TodoList : TodoListAPI {
     
+    let defaultDatabasePath = "/Users/sakandur/sai/sqlite-autoconf-3130000/todolist"
+    
+    let designName = "TodoList-SQLite"
+    
+    var sqlLite: SQLite?
+    
+    internal init(databasePath: String) {
+        do {
+          sqlLite = try  SQLite(path: databasePath)
+        }
+        catch{
+            print("Error opening the database")
+        }
+    }
+    
+    internal init(){
+        do {
+            sqlLite = try  SQLite(path: defaultDatabasePath)
+        }
+        catch{
+            print("Error opening the database")
+        }
+        
+    }
+    
     func count(withUserID: String?, oncompletion: (Int?, ErrorProtocol?) -> Void) {
-        // TODO:
+        let user = withUserID ?? "default"
+        do {
+            let query = "SELECT * FROM todos WHERE owner_id=\"\(user)\""
+            let results = try sqlLite?.execute(query)
+            oncompletion(results?.count, nil)
+        }
+        catch {
+            Log.error("There was a problem with the SQLite query: \(error)")
+            oncompletion(nil, TodoCollectionError.CreationError("There was a problem with the MySQL query: \(error)"))
+        }
     }
     
     func clear(withUserID: String?, oncompletion: (ErrorProtocol?) -> Void) {
-        // TODO:
+        let user = withUserID ?? "default"
+        do {
+            let query = "DELETE FROM todos WHERE owner_id=\"\(user)\""
+            _ = try sqlLite?.execute(query)
+            oncompletion(nil)
+        }
+        catch {
+            Log.error("There was a problem with the MySQL query: \(error)")
+            oncompletion(TodoCollectionError.CreationError("There was a problem with the MySQL query: \(error)"))
+        }
     }
     
     func clearAll(oncompletion: (ErrorProtocol?) -> Void) {
-        // TODO:
+        do {
+            let query = "DELETE From todos"
+            _ = try sqlLite?.execute(query)
+            oncompletion(nil)
+        }
+        catch {
+            Log.error("There was a problem with the MySQL query: \(error)")
+            oncompletion(TodoCollectionError.CreationError("There was a problem with the MySQL query: \(error)"))
+        }
     }
     
     func get(withUserID: String?, oncompletion: ([TodoItem]?, ErrorProtocol?) -> Void) {
-        // TODO:
+        let user = withUserID ?? "default"
+        do {
+            let query = "SELECT * FROM todos WHERE owner_id=\"\(user)\""
+            let results = try sqlLite?.execute(query)
+            let todos = try parseTodoItemList(results: results!)
+            oncompletion(todos, nil)
+        }
+        catch {
+            Log.error("There was a problem with the MySQL query: \(error)")
+            oncompletion(nil, TodoCollectionError.CreationError("There was a problem with the MySQL query: \(error)"))
+        }
     }
     
     func get(withUserID: String?, withDocumentID: String, oncompletion: (TodoItem?, ErrorProtocol?) -> Void ) {
-        // TODO:
+        let user = withUserID ?? "default"
+        do {
+            let query = "SELECT * FROM todos WHERE owner_id=\"\(user)\" AND tid=\"\(withDocumentID)\""
+            let results = try sqlLite?.execute(query)
+            let todos = try parseTodoItemList(results: results!)
+            oncompletion(todos[0], nil)
+        }
+        catch {
+            Log.error("There was a problem with the MySQL query: \(error)")
+            oncompletion(nil, TodoCollectionError.CreationError("There was a problem with the MySQL query: \(error)"))
+        }
     }
     
     func add(userID: String?, title: String, order: Int, completed: Bool,
              oncompletion: (TodoItem?, ErrorProtocol?) -> Void ) {
-        // TODO:
+        let user = userID ?? "default"
+        do {
+            let completedValue = completed ? 1 : 0
+            let query = "INSERT INTO todos (title, owner_id, completed, orderno) VALUES ( \"\(title)\", \"\(user)\", \(completedValue), \(order))"
+            _ = try sqlLite?.execute(query)
+            let result = try sqlLite?.execute("SELECT last_insert_rowid()")
+            guard result?.count == 1 else {
+                oncompletion(nil, TodoCollectionError.IDNotFound("There was a problem adding a TODO item"))
+                return
+            }
+            guard let documentID = result?[0].data["last_insert_rowid()"]
+                where Int(documentID) > 0 else {
+                    oncompletion(nil, TodoCollectionError.IDNotFound("There was a problem adding a TODO item"))
+                    return
+            }
+            let todoItem = TodoItem(documentID: String(documentID), userID: user, order: order, title: title, completed: completed)
+            oncompletion(todoItem, nil)
+        }
+        catch {
+            Log.error("There was a problem with the SQlLite query: \(error)")
+            oncompletion(nil, TodoCollectionError.CreationError("There was a problem with the MySQL query: \(error)"))
+        }
     }
     
     func update(documentID: String, userID: String?, title: String?, order: Int?,
                 completed: Bool?, oncompletion: (TodoItem?, ErrorProtocol?) -> Void ) {
-        // TODO:
+        let user = userID ?? "default"
+        
+        var originalTitle: String = "", originalOrder: Int = 0, originalCompleted: Bool = false
+        var titleQuery: String = "", orderQuery: String = "", completedQuery: String = ""
+        
+        if title == nil || order == nil || completed == nil {
+            
+            get(withUserID: userID, withDocumentID: documentID){
+                todo, error in
+                
+                if let todo = todo {
+                    originalTitle = todo.title
+                    originalOrder = todo.order
+                    originalCompleted = todo.completed
+                }
+            }
+        }
+        
+        let finalTitle = title ?? originalTitle
+        if (title != nil) {
+            titleQuery = " title=\"\(finalTitle)\","
+        }
+        
+        let finalOrder = order ?? originalOrder
+        if (order != nil) {
+            orderQuery = " orderno=\(finalOrder),"
+        }
+        
+        let finalCompleted = completed ?? originalCompleted
+        if (completed != nil) {
+            let completedValue = finalCompleted ? 1 : 0
+            completedQuery = " completed=\(completedValue),"
+        }
+        
+        var concatQuery = titleQuery + orderQuery + completedQuery
+        
+        do {
+            let query = "UPDATE todos SET" + String(concatQuery.characters.dropLast()) + " WHERE tid=\"\(documentID)\""
+            _ = try sqlLite?.execute(query)
+            
+            let todoItem = TodoItem(documentID: String(documentID), userID: user, order: finalOrder, title: finalTitle, completed: finalCompleted)
+            oncompletion(todoItem, nil)
+            
+        }
+        catch {
+            Log.error("There was a problem with the MySQL query: \(error)")
+            oncompletion(nil, TodoCollectionError.CreationError("There was a problem with the MySQL query: \(error)"))
+        }
     }
     
     func delete(withUserID: String?, withDocumentID: String, oncompletion: (ErrorProtocol?) -> Void) {
-        // TODO:
+        let user = withUserID ?? "default"
+        
+        do {
+            let query = "DELETE FROM todos WHERE owner_id=\"\(user)\" AND tid=\"\(withDocumentID)\""
+            _ = try sqlLite?.execute(query)
+            
+            oncompletion(nil)
+            
+        }
+        catch {
+            Log.error("There was a problem with the MySQL query: \(error)")
+            oncompletion(TodoCollectionError.IDNotFound("There was a problem with the MySQL query: \(error)"))
+        }    }
+    
+    private func parseTodoItemList(results: [SQLite.Result.Row]) throws -> [TodoItem] {
+        
+        var todos = [TodoItem]()
+        for row in results {
+            let item: TodoItem = try createTodoItem(entry: row.data)
+            todos.append(item)
+        }
+        return todos
+    }
+
+    private func createTodoItem(entry: [String : String]) throws -> TodoItem {
+        let completedValue = Int(entry["completed"]!) == 1 ? true : false
+        let todoItem = TodoItem(documentID: String(entry["tid"]!), userID: String(entry["owner_id"]!), order: (Int(entry["orderno"]!))!, title: String(entry["title"]!), completed: completedValue)
+        return todoItem
     }
     
 }
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,18 +1,4 @@
-/**
- * Copyright IBM Corporation 2016
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
+
 
 import Foundation
 
@@ -23,40 +9,47 @@ import TodoListWeb
 import CloudFoundryEnv
 import TodoListAPI
 
-
 Log.logger = HeliumLogger()
-
 
 extension DatabaseConfiguration {
     
     init(withService: Service) {
-        
         if let credentials = withService.credentials{
-            self.host     = credentials["host"].stringValue
+            self.host = credentials["host"].stringValue
             self.username = credentials["username"].stringValue
             self.password = credentials["password"].stringValue
-            self.port     = UInt16(credentials["port"].stringValue)!
+            self.port = UInt16(credentials["port"].stringValue)!
         } else {
             self.host = "127.0.0.1"
-            self.username = nil
-            self.password = nil
-            self.port = UInt16(5984)
+            self.username = "root"
+            self.password = ""
+            self.port = UInt16(3306)
         }
-        self.options = [:]
+        self.options = ["test" : "test"]
     }
 }
 
-if let service = try CloudFoundryEnv.getAppEnv().getService(spec: "TodoList-Cloudant"){
-    let _ = DatabaseConfiguration(withService: service)
+let databaseConfiguration: DatabaseConfiguration
+let todos: TodoList
+
+do {
+    if let service = try CloudFoundryEnv.getAppEnv().getService(spec: "TodoList-SQLite"){
+        Log.verbose("Found TodoList-MySQL")
+        databaseConfiguration = DatabaseConfiguration(withService: service)
+        todos = TodoList(databasePath: "Somepath")
+    } else {
+        Log.info("Could not find Bluemix SQlLite service")
+        todos = TodoList(databasePath: "/Users/sakandur/sai/sqlite-autoconf-3130000/todolist")
+    }
+    
+    let controller = TodoListController(backend: todos)
+    
+    let port = try CloudFoundryEnv.getAppEnv().port
+    Log.verbose("Assigned port is \(port)")
+    
+    Kitura.addHTTPServer(onPort: port, with: controller.router)
+    Kitura.run()
+    
+} catch CloudFoundryEnvError.InvalidValue {
+    Log.error("Oops... something went wrong. Server did not start.")
 }
-else {
-    Log.info("Using local development environment")
-}
-
-let todos: TodoListAPI = TodoList()
-
-
-let controller = TodoListController(backend: todos)
-
-Kitura.addHTTPServer(onPort: 8090, with: controller.router)
-Kitura.run()
