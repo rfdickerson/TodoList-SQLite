f968687199f76e619661a3bdbf3b5e958a6ecfa3
Jessica Pittman <jpittma@us.ibm.com>
2016-11-22 09:34:25 -0600

    Update to swift 3.0.1 (#12)
    * Updated to kitura 1.2
    
    * Updated .swift-version file to 3.0.1

--- Package.swift
+++ Package.swift
@@ -12,9 +12,9 @@ let package = Package(
         )
     ],
     dependencies: [
-         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 7),
-         .Package(url: "https://github.com/IBM-Swift/Kitura.git",majorVersion: 1, minor: 0),
-         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",majorVersion: 1, minor: 0),
+         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 8),
+         .Package(url: "https://github.com/IBM-Swift/Kitura.git",majorVersion: 1, minor: 2),
+         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",majorVersion: 1, minor: 1),
          .Package(url: "https://github.com/rfdickerson/sqlite.git",majorVersion: 0, minor: 3)
     ]
 )
